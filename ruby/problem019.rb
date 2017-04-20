f6a887587f0977fc5f5a310a92e74c0a95c0fcee
thekeenant <keenan@keenant.com>
2017-04-20 14:59:03 -0500

    Ruby 019

---
+++ ruby/problem019.rb
@@ -0,0 +1,21 @@
+load 'euler_problem.rb'
+require 'date'
+
+class Problem019
+  include EulerProblem
+
+  def run
+    count = 0
+    (1901..2000).each do |year|
+      (1..12).each do |month|
+        day = Date.new(year, month, 1).wday
+        if day == 0
+          count += 1
+        end
+      end
+    end
+    count
+  end
+end
+
+execute(Problem019.new)
