838095976aeceeaa4e81c0206fdb9e8132245fb4
thekeenant <keenan@keenant.com>
2017-04-21 22:52:00 -0500

    My own fibs sequence
    Very slow but this actually makes sense to me

--- haskell/problem002.hs
+++ haskell/problem002.hs
@@ -1,10 +1,10 @@
--- fib :: Integer -> Integer
--- fib 0 = 0
--- fib 1 = 1
--- fib n = fib (n - 1) + fib (n - 2)
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
 
 fibs :: [Integer]
-fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+fibs = map fib [0..]
 
 solution = sum (takeWhile (<= 4000000) (filter even fibs))
 
