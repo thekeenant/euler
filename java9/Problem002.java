f1e57e123c53e818d8c33a15d0f28624dd89af02
thekeenant <keenan@keenant.com>
2017-04-23 00:47:17 -0500

    002 java 9 fastFibs

--- java9/Problem002.java
+++ java9/Problem002.java
@@ -1,4 +1,7 @@
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Problem002 implements EulerSolution {
     public static void main(String[] args) {
@@ -8,7 +11,27 @@ public class Problem002 implements EulerSolution {
     @Override
     public long run() {
         // takeWhile(...) was included in Java 9 SDK, not Java 8 for some reason.
-        return fibs().takeWhile(i -> i < 4_000_000).filter(i -> i % 2 == 0).sum();
+
+        // ~20ms
+        return fastFibs().takeWhile(i -> i < 4_000_000).filter(i -> i % 2 == 0).sum();
+
+        // ~80ms
+        // return fibs().takeWhile(i -> i < 4_000_000).filter(i -> i % 2 == 0).sum();
+    }
+
+    private IntStream fastFibs() {
+        return Stream.generate(new Supplier<Integer>() {
+            private int n1 = 0;
+            private int n2 = 1;
+
+            @Override
+            public Integer get() {
+                int result = n1 + n2;
+                n1 = n2;
+                n2 = result;
+                return result;
+            }
+        }).mapToInt(Integer::intValue);
     }
 
     private IntStream fibs() {
