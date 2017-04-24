268c5669695d3788ace086b846cc3b3f0cb2a9d3
thekeenant <keenan@keenant.com>
2017-04-24 11:48:54 -0500

    Kotlin: 016, class extensions

---
+++ kotlin/Problem016.kt
@@ -0,0 +1,17 @@
+import java.math.BigInteger
+import java.util.stream.IntStream
+
+fun main(args: Array<String>) {
+    EulerSolution.execute(Problem016())
+}
+
+// cool class extension
+fun Number.digits() : IntStream {
+    return this.toString().chars().map(Character::getNumericValue)
+}
+
+class Problem016 : EulerSolution {
+    override fun run(): Long {
+        return BigInteger.valueOf(2).pow(1000).digits().sum().toLong()
+    }
+}
