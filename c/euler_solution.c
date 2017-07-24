c63c69d580665331710409b67710fa72a568d9fd
thekeenant <keenan@keenant.com>
2017-07-24 16:48:18 -0500

    Properly calculate elapsed time for C
    Take into account seconds and microseconds elapsed

--- c/euler_solution.c
+++ c/euler_solution.c
@@ -1,13 +1,20 @@
 #include "euler_solution.h"
 
+double timevalToMs(struct timeval time) {
+ return (double) (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
+
 void execute(long long (*solution)()) {
     struct timeval start, stop;
     gettimeofday(&start, NULL);
     long long result = solution();
     gettimeofday(&stop, NULL);
 
+    double startMs = timevalToMs(start);
+    double stopMs = timevalToMs(stop);
+
     printf("Result: %llu\n", result);
-    printf("Time: %lums\n", (stop.tv_usec - start.tv_usec) / 1000);
+    printf("Time: %dms\n", (int) (stopMs - startMs));
 }
 
 int main() {
