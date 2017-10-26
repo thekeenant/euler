050fbc3e305c34415c1062661edde810c56cd147
thekeenant <keenan@keenant.com>
2017-10-26 12:35:52 -0500

    unnecessary stuff

--- go/problem_001.go
+++ go/problem_001.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var sum int
+	sum := 0
 
 	for i := 1; i < 1000; i++ {
 		if i % 5 == 0 || i % 3 == 0 {
@@ -11,5 +11,5 @@ func main() {
 		}
 	}
 
-	fmt.Print(sum);
+	fmt.Print(sum)
 }
